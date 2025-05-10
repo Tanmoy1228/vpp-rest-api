7c8b99d4a75598aa023e83affd541064c0ae07d2
Tanmoy1228 <tanmoy1228@gmail.com>
2025-05-10 17:10:59 +0600

    chore: initialize vpp-rest-api Spring Boot project with basic structure and web dependency
    - Generated Spring Boot application using Spring Initializr
    - Added Web and Test dependencies

---
+++ src/main/java/com/tanmoy/vpp/VppRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.tanmoy.vpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VppRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VppRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanmoy/vpp/VppRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanmoy.vpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VppRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
